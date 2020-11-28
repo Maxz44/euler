1e1e36513511bbfb54d962b9f8fbfc6c10e37d8e
max <maxime.zaremba@outlook.fr>
2020-11-29 00:29:00 +0100

    misc amelioration

--- euler.py
+++ euler.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
 def euler1(n):
     return sum([x for x in range(1, n) if x % 3 == 0 or x % 5 == 0])
 
-#euler1(1000)
+
+# euler1(1000)
+
 
 def fib(n):
-    if ( n > 3):
+    if (n > 3):
         fib = [1, 1]
         i = 0
         for i in range(n - 2):
@@ -16,31 +20,39 @@ def fib(n):
         else:
             print('chose an integer value')
 
+
 def euler2(n):
-    fib = [1, 1]
+    fib = [1, 2]
     i = 0
-    while fib[-1] < 4000000:
+    while fib[-1] < n:
         fib.append(fib[i] + fib[i+1])
         i += 1
     return sum([x for x in fib if x % 2 == 0])
 
-#euler2(4000000)
+
+# print(euler2(4000000))
+
 
 def isprime(n):
     hasfactor = False
     i = 2
     from math import sqrt
-    while not hasfactor and i < int(sqrt(n)) + 1:
+    while not hasfactor and i < int(sqrt(n)):
         hasfactor = n % i == 0
         i += 1
     return not hasfactor
 
-def euler3(n):
-    fact = []
-    from math import sqrt
-    for i in range(1, int(sqrt(n))+1):
-        if n % i == 0 and isprime(i):
-            fact.append(i)
-    return fact[-1]
 
-#euler3(1)
+def euler3(n):
+    lastfact = 1
+    from math import sqrt
+    for i in range(3, int(sqrt(n))):
+        if n % i == 0 and isprime(i):
+            lastfact = i
+    return lastfact
+
+
+print(euler3(600851475143))
+
+def euler4():
+    pass
