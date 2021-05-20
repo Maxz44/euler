2819287b47f05d17c3c9ccf1c7dd08156f95a17e
Maxime Zaremba <Maxz44@users.noreply.github.com>
2021-05-20 17:23:58 +0200

    Solved pb 4

--- euler.py
+++ euler.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+# --- Euler pb 1
+
 def euler1(n):
     return sum([x for x in range(1, n) if x % 3 == 0 or x % 5 == 0])
 
 
 # euler1(1000)
 
+# --- Euler pb 2
 
 def fib(n):
     if (n > 3):
@@ -32,6 +35,7 @@ def euler2(n):
 
 # print(euler2(4000000))
 
+# --- Euler pb 3
 
 def isprime(n):
     hasfactor = False
@@ -52,7 +56,23 @@ def euler3(n):
     return lastfact
 
 
-print(euler3(600851475143))
+# print(euler3(600851475143))
 
-def euler4():
-    pass
+# --- Euler pb 4
+
+# A palindromic number reads the same both ways. The largest palindrome made
+# from the product of two 2-digit numbers is 9009 = 91 × 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def nb_is_palindrom(n):
+    return str(n) == str(n)[::-1]
+
+def euler4(n):
+    rslt = set()
+    for i in range(10**(n-1), 10**n):
+        for j in range(10**(n-1), 10**n):
+            rslt.add(i*j)
+    rslt = [x for x in rslt if nb_is_palindrom(x)]
+    return max(rslt)
+
+print(euler4(3))
